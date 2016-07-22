0c6c285d9360416688d1cba59a9abd5be6426e34
Elodia-Elwen <elodia-engel@list.ru>
2016-07-22 23:09:55 +0300

    Ура! Решил issue 1!

--- task_dm2.py
+++ task_dm2.py
@@ -4,9 +4,14 @@ from time import sleep
 def f(x):
     return x*x
 
+print (drawman_scale)
+drawman_scale = 40
+x = -5.0
+to_point (x, f(x))
 pen_down()
-for x in range (0,11):
-    to_point(x,f(x))
+while x <= 5:
+    to_point(x, f(x))
+    x += 0.1
 pen_up()
 
 sleep (20)
